3c3c39bfbc4cb441ba79796bbad519aaf1cf5669
hahuyhungdev <hahuyhungdev@gmail.com>
2022-11-27 16:29:59 +0700

    update feature

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { BsSun, BsFillMoonFill } from "react-icons/bs";
+import { Link, Outlet } from "react-router-dom";
 
 import { toggleTheme } from "../../features/theme/themeSlice";
 import { useAppDispatch, useAppSelector } from "../../hooks/storeHook";
@@ -11,9 +12,9 @@ const Header = () => {
     <header className="mb-20">
       <nav className="border-b border-gray-200 border-opacity-25 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="#!" className="flex items-center">
+          <Link to="/" className="flex items-center">
             <span className="self-center text-xl font-semibold whitespace-nowrap">Movies</span>
-          </a>
+          </Link>
 
           <div className="flex items-center lg:order-2">
             {darkTheme.theme && <BsSun onClick={() => onToggle()} className="hover:opacity-50 cursor-pointer" />}
@@ -23,6 +24,7 @@ const Header = () => {
           </div>
         </div>
       </nav>
+      <Outlet />
     </header>
   );
 };
--- src/features/movies/movieSlice.ts
+++ src/features/movies/movieSlice.ts
@@ -12,16 +12,34 @@ export const getMovies = createAsyncThunk("movies/getMovies", async (data, thunk
   }
 });
 
+// get move details
+export const getMovieDetails = createAsyncThunk(
+  "movies/getMovieDetails",
+  async (movieId: string | undefined, thunkApi) => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=a8a6fa2f944128
+e9712135bc3ca000b1`
+      );
+      return await response.json();
+    } catch (error: any) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
 interface MovieState {
   loading: boolean;
   error: null | string;
   data: null | { results: any[] };
+  movieDetails: null | any;
 }
 
 const initialState: MovieState = {
   loading: false,
   error: null,
   data: null,
+  movieDetails: null,
 };
 
 // Slice
@@ -41,7 +59,18 @@ const movieSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     });
+    builder.addCase(getMovieDetails.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getMovieDetails.fulfilled, (state, action: PayloadAction<any>) => {
+      state.loading = false;
+      state.movieDetails = action.payload;
+    });
+    builder.addCase(getMovieDetails.rejected, (state, action: PayloadAction<any>) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
   },
 });
-
+export const getSelectedMovie = (state: any) => state.movies.movieDetails;
 export default movieSlice.reducer;
